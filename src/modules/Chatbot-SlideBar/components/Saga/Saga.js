4c12c17c7e062c57b6d814a0b177dbed65509b4d
Mesbahi ALi <aliimesbahi31@gmail.com>
2023-09-19 14:27:09 +0100

    last update

--- src/modules/Chatbot-SlideBar/components/Saga/Saga.js
+++ src/modules/Chatbot-SlideBar/components/Saga/Saga.js
@@ -1,17 +1,14 @@
 import { put, call, select } from 'redux-saga/effects';
-import { instance } from "../../../../common/Axios"; // Assuming you have this axios instance
+import { UploadServer } from '../../../../common/services/UploadService';
 
 function* sendFilesToServerSaga(action) {
   try {
       const files = yield select(state => state.upload.files);
-      const formData = new FormData();
-      files.forEach(file => {
-          formData.append('file', file);
-      });
-      const response = yield call([instance, instance.post], '/upload', formData);
+      const response = yield call(UploadServer, files);
       console.log('Response:', response);
   } catch (error) {
       console.error('Error uploading files:', error.response ? error.response : error);
   }
 }
+
 export default sendFilesToServerSaga;
