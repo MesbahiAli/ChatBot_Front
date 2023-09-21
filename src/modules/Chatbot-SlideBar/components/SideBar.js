8b48b9ebae125b0a0ff2a82f56cb1c1245f6cf70
Mesbahi ALi <aliimesbahi31@gmail.com>
2023-09-21 18:39:41 +0100

    last update

--- src/modules/Chatbot-SlideBar/components/SideBar.js
+++ src/modules/Chatbot-SlideBar/components/SideBar.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button, IconButton, List, ListItem, ListItemText, ListItemSecondaryAction } from '@mui/material';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 import DeleteIcon from '@mui/icons-material/Delete';
@@ -14,10 +14,28 @@ import { deleteFileApi } from '../../../common/services/DeleteService';
 import { requestSummarize } from '../../Chatbot-RightBar/components/Summarize/State/SummarizeActions';
 import { fetchPdfRequest } from './FileView.js/State/ViewActions';
 import CloudDoneIcon from '@mui/icons-material/CloudDone';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 // 20/09/23
 
-import MenuIcon from '@mui/icons-material/Menu'; 
-import JwtUtils from '../../../routing/JwtUtils';
+
+// Mouad Doadi - 21/09/23 >
+
+import SummarizeIcon from '@mui/icons-material/Summarize';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+
+// toggle >
+
+import ToggleButton from '@mui/lab/ToggleButton';
+import ToggleButtonGroup from '@mui/lab/ToggleButtonGroup';
+
+// < toggle
+
+// < 21/09/23
+
+import MenuIcon from '@mui/icons-material/Menu';
+import JwtUtils from '../../../routing/JwtUtils'; /* TAHA */
+import SettingsPanel from './SettingsPanel';
+
 const UploadComponent = () => {
 
   const [selectedFilesFromServer, setSelectedFilesFromServer] = useState([]);
@@ -35,15 +53,19 @@ const UploadComponent = () => {
   const checkStyle = {
     color: '#fff',
     margin: 0,
-    padding: 0
+    padding: 0,
+    position:'relative'
   };
 
   const sendServerButton = {
     color: '#fff'
   };
+
+  /* TAHA */
+
   const disabledButton = {
-    backgroundColor:"grey",
-    cursor:""
+    backgroundColor: "grey",
+    cursor: ""
   };
 
   const sendServerLabel = {
@@ -53,19 +75,20 @@ const UploadComponent = () => {
     width: '100%',  // Take full width of its parent.
     height: '100%', // Take full height of its parent.
   };
-  
+
 
   const docStyle = {
     color: 'white',
     display: 'flex',
     alignItems: 'center',
+    justifyContent: "flex-start"
   };
 
   const listItemStyle = {
     padding: 0,
     margin: 0,
-    marginRight: '-80%',
-    
+    // marginRight: '-80%',
+
   };
 
   const docDeleteStyle = {
@@ -75,9 +98,9 @@ const UploadComponent = () => {
   // ---------------------------
 
   // 20/09/23
-
+  const [open,setOpen] = useState(false);
   const [isSidebarOpen, setIsSidebarOpen] = React.useState(true);
-  
+
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
@@ -100,12 +123,22 @@ const UploadComponent = () => {
 
   //
 
+  // Mouad Doadi - 21/09/23 >
+
+  const [selected, setSelected] = useState(false);
+
+  // < 21/09/23
+
   const dispatch = useDispatch();
   const filesUpload = useSelector(state => state.upload.files);
+  console.log(filesUpload)
   const files = useSelector(state => state.Files.data);
+
+  /* TAHA */
 
   const isUploaded = filesUpload?.length !== 0;
   const isSelected = selectedFilesFromServer?.length !== 0;
+
   useEffect(() => {
     dispatch(fetchDataRequest());
   }, []);
@@ -130,89 +163,95 @@ const UploadComponent = () => {
     dispatch(sendFileNamesRequest(selectedFilesFromServer));
   };
 
-  const handleDelete = (filename) => {
-    deleteFileApi(filename)
-      .then(response => {
-        dispatch(fetchDataRequest());  
-      })
-      .catch(error => {
-        console.error("Error deleting file:", error);
-      });
-  };
-   const handleLogout = () => {
+  
+  /* TAHA */ const handleLogout = () => {
     JwtUtils.logOut();
   };
-    return (
+  return (
     <>
-    <IconButton style={{ color: '#000', backgroundColor: '#343e8f', margin: 0, padding: '5px', borderRadius: 0 }} onClick={toggleSidebar} className="hamburger-icon" sx={{ display: { xs: 'flex', md: 'none', xl: 'none' } }}>
+      <IconButton style={{ color: '#000', backgroundColor: '#343e8f', margin: 0, padding: '5px', borderRadius: 0 }} onClick={toggleSidebar} className="hamburger-icon" sx={{ display: { xs: 'flex', md: 'none', xl: 'none' } }}>
         <MenuIcon style={{ color: '#aaa', margin: 0, padding: 0 }} />
       </IconButton>
-    <div className={isSidebarOpen ? "SidebarDev open" : "SidebarDev closed"} sx={{ display: { xs: 'none', sm: 'block' } }}>
+      <div className={isSidebarOpen ? "SidebarDev open" : "SidebarDev closed"} >
         <input
-            style={{ display: 'none' }}
-            id="file-input"
-            multiple
-            type="file"
-            onChange={handleFileChange}
-          />
-          <label id='uploadLabel' htmlFor="file-input">
-            <Button className="sidebar-button" component="span" startIcon={<CloudDoneIcon />} style={buttonStyle} >
-              <span className="text">Select Files</span>
-            </Button>
-          </label>
+          style={{ display: 'none' }}
+          id="file-input"
+          multiple
+          type="file"
+          onChange={handleFileChange}
+        />
+        <label id='uploadLabel' htmlFor="file-input">
+          <Button className="sidebar-button" component="span" startIcon={<CloudDoneIcon />} style={buttonStyle} >
+            <span className="text">Select Files</span>
+          </Button>
+        </label>
 
-          <div className="file-list-container">
-            <List>
+        <div className="file-list-container">
+          <List>
 
-              {filesUpload.map((file, index) => (
-                <ListItem id='listItem' style={docStyle} key={index}>
-                  <ListItemText primary={file.name} id='listItemSecondaryAction' style={listItemStyle}>
-                  </ListItemText>
-                  {/* <ListItemText primary={file.name} id='listItemPrimaryText'/> */}
-                </ListItem>
-              ))}
-            </List>
-          </div>
-
-          <label id='sendServerLabel'>
-            <Button onClick={handleUploadButtonClick} id='sendBtn' disabled={isUploaded ? false : true} style={isUploaded ? sendServerButton : disabledButton}  endIcon={<CloudUploadIcon />}>
-              <span className="text">Upload</span>
-            </Button>
-          </label>
-    
-          <label id='sendServerLabel'>
-        <Button  onClick={handleSendSelectedFilesFromServer} id='sendBtn' disabled={isSelected ? false : true} style={isSelected ? sendServerButton : disabledButton} endIcon={<SendIcon />} >
-          <span className="text">Send selected files</span>
-        </Button>
-      </label>
-    
-          <Button onClick={()=>dispatch(requestToggle())}>Change language</Button>
-          <List> {files?.map(el => (
-        <div key={el} style={{ display: 'flex' }}>
-                    {el}
-
-          <Checkbox 
-            style={checkStyle}
-            edge="start"
-            checked={selectedFilesFromServer.includes(el)}
-            onChange={() => handleServerFileSelect(el)}
-          />
-          <IconButton style={checkStyle} edge="end" onClick={() => handleDelete(el)} id='deleteIcon'>
-          <DeleteIcon />
-      </IconButton>
-      <Button onClick={() => dispatch(requestSummarize(el))}>
-            Summarize
-        </Button>
-      
-      <Button onClick={()=>dispatch(fetchPdfRequest(el))}>view</Button>
-
-      
-
+            {filesUpload.map((file, index) => (
+              <ListItem id='listItem' style={docStyle} key={index}>
+                <ListItemText primary={file.name} id='listItemSecondaryAction' style={listItemStyle}>
+                </ListItemText>
+                {/* <ListItemText primary={file.name} id='listItemPrimaryText'/> */}
+              </ListItem>
+            ))}
+          </List>
         </div>
-      ))}
-      </List>
-      {JwtUtils.isActif()?<Button onClick={handleLogout}>logout</Button>:null}
-    </div>
+
+        <label id='sendServerLabel'>
+          {/* <Button onClick={handleUploadButtonClick} id='sendBtn' style={sendServerButton} endIcon={<CloudUploadIcon />}>  TAHA */}
+          <Button onClick={handleUploadButtonClick} id='sendBtn' disabled={isUploaded ? false : true} style={isUploaded ? sendServerButton : disabledButton} endIcon={<CloudUploadIcon />}>
+            <span className="text">Upload</span>
+          </Button>
+        </label>
+
+
+
+        {/* <br />
+          <hr style={ { height: '1px', border: 'none' } }/>
+    
+          
+
+          <List className="second-list file-list-container"> {files?.map(el => (
+            <div key={el} style={{ display: 'flex' }} className='li-container'>
+                    {el} */}
+        <label id='sendServerLabel'>
+          <Button onClick={handleSendSelectedFilesFromServer} id='sendBtn' disabled={isSelected ? false : true} style={isSelected ? sendServerButton : disabledButton} endIcon={<SendIcon />} >
+            <span className="text">Send selected files</span>
+          </Button>
+        </label>
+
+        <Button onClick={() => dispatch(requestToggle())}>Change language</Button>
+        <List> {files?.map(el => (
+          <div key={el} style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '5px 3px', paddingLeft: '5px', marginBottom: '10px', backgroundColor: '#212229', borderRadius: '8px' }}>
+            {el}
+
+            <div className='icon-container'>
+              <Checkbox
+                style={checkStyle}
+                edge="start"
+                checked={selectedFilesFromServer.includes(el)}
+                onChange={() => handleServerFileSelect(el)}
+              />
+              <SettingsPanel el={el}/>
+
+            </div>
+
+          </div>
+        ))}
+        </List>
+
+        {JwtUtils.isActif() ? <Button onClick={handleLogout}>logout</Button> : null}
+
+
+        <label id='sendServerLabel'>
+          <Button onClick={handleSendSelectedFilesFromServer} id='sendBtn' disabled={isSelected ? false : true} style={isSelected ? sendServerButton : disabledButton} endIcon={<SendIcon />} >
+            <span className="text">Send selected files</span>
+          </Button>
+        </label>
+
+      </div>
     </>
   );
 };
