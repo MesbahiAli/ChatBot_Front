4c520473ab022741e10ea09506c937123535c04d
AiAutomationAgency <143697640+AiAutomationAgency@users.noreply.github.com>
2023-09-28 09:41:12 +0100

    Working on the toggle language functionnality

--- src/modules/Chatbot-RightBar/components/DropupMenu.js
+++ src/modules/Chatbot-RightBar/components/DropupMenu.js
@@ -1,17 +1,26 @@
-
 import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 
 const DropUpMenu = () => {
+    const options = [
+        { src: "https://www.svgrepo.com/show/248851/united-states.svg", alt: "EN" },
+        { src: "https://www.svgrepo.com/show/248840/netherlands.svg", alt: "Dutch" },
+        { src: "https://www.svgrepo.com/show/508678/flag-wf.svg", alt: "FR" }
+    ];
+
     const [anchorEl, setAnchorEl] = useState(null);
+    const [selectedItem, setSelectedItem] = useState(options[0]); // Default to the first image
 
     const handleClick = (event) => {
         setAnchorEl(event.currentTarget);
     };
 
-    const handleClose = () => {
+    const handleClose = (option) => {
+        if (option) {
+            setSelectedItem(option);
+        }
         setAnchorEl(null);
     };
 
@@ -21,14 +30,14 @@ const DropUpMenu = () => {
                 variant="contained"
                 color="primary"
                 onClick={handleClick}
-                style={{ position: 'absolute', bottom: 0 }}
+                style={{ position: 'absolute', bottom: 0, display: 'flex', alignItems: 'center' }}
             >
-                Open Menu
+                <img className='langImg' src={selectedItem.src} alt={selectedItem.alt} width="30" />
             </Button>
             <Menu
                 anchorEl={anchorEl}
                 open={Boolean(anchorEl)}
-                onClose={handleClose}
+                onClose={() => handleClose()}
                 anchorOrigin={{
                     vertical: 'top',
                     horizontal: 'center',
@@ -39,9 +48,11 @@ const DropUpMenu = () => {
                 }}
                 keepMounted
             >
-                <MenuItem onClick={handleClose}><img className='langImg' src="https://www.svgrepo.com/show/248851/united-states.svg" alt="EN" width="30" /></MenuItem>
-                <MenuItem onClick={handleClose}><img className='langImg' src="https://www.svgrepo.com/show/248840/netherlands.svg" alt="Dutch" width="30" /></MenuItem>
-                {/* <MenuItem onClick={handleClose}>Option 3</MenuItem> */}
+                {options.map((option, index) => (
+                    <MenuItem key={index} onClick={() => handleClose(option)}>
+                        <img className='langImg' src={option.src} alt={option.alt} width="30" />
+                    </MenuItem>
+                ))}
             </Menu>
         </div>
     );
