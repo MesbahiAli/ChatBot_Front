bfc1870dcb86afde050c57c0cdab2c57cbaef0f4
Taha Bourichi <bourichi.taha@gmail.com>
2023-10-03 12:43:38 +0100

    created on scroll animation and added on enter event to textfield

--- src/modules/Home/components/Navbar.jsx
+++ src/modules/Home/components/Navbar.jsx
@@ -10,30 +10,29 @@ import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { selectSidebarOpen } from '../state/ReducerHome';
 import { Link } from 'react-router-dom';
-import JwtUtils from '../../../routing/JwtUtils'; 
+import JwtUtils from '../../../routing/JwtUtils';
 
-  const handleLogout = () => {
-    JwtUtils.logOut();
+const handleLogout = () => {
+  JwtUtils.logOut();
 };
 
 const Navbar = () => {
   const location = useLocation();
   const isSidebarOpen = useSelector(selectSidebarOpen);
 
-
   return (
-    <div className="navbar-container">
+    <div className={"navbar-container "} >
       <div className={location.pathname === "/Chatbot" ? (isSidebarOpen ? "nc-logo-container open" : "nc-logo-container ml-80") : "nc-logo-container"}>
         <img src={logo} alt="" className='nc-lc-logo' />
       </div>
       <div className="nc-buttons-container">
-      <Link to="/Chatbot">
-          <Button variant="contained" className='nc-bc-button'  startIcon={<ChatOutlinedIcon />}>
-          Chatbot
-        </Button>
+        <Link to="/Chatbot">
+          <Button variant="contained" className='nc-bc-button' startIcon={<ChatOutlinedIcon />}>
+            Chatbot
+          </Button>
         </Link>
-        {JwtUtils.isActif() ?  <Button onClick={handleLogout} variant="contained" className='nc-bc-button'  startIcon={<LogoutIcon />}> Logout</Button> : null}
-                      </div>
+        {JwtUtils.isActif() ? <Button onClick={handleLogout} variant="contained" className='nc-bc-button' startIcon={<LogoutIcon />}> Logout</Button> : null}
+      </div>
     </div>
   )
 }
